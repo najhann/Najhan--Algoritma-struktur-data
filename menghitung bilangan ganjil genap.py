5b7fb350e0c56a22a7fa033b4eebdc3722e7ee2b
najhann <162579224+najhann@users.noreply.github.com>
2024-09-15 14:28:44 +0800

    Add files via upload

---
+++ menghitung bilangan ganjil genap.py
@@ -0,0 +1,15 @@
+def (2)
+"""cek apakah bilangan genap atau ganjil."""
+if % 2==0:
+    return f"{2}adalah bilagan genap."
+else:
+    return f"{2}adalah bilangan ganjil."
+    def main():
+        while true:
+            try:
+                2= unt(input(2))
+                hasil = cek_genap_genajil(2)
+                print(hasil)
+                expect valueError
+                print(2)
+                2
